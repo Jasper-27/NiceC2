d661a97f82234a31a4ee9e47c055e1c46ade978a
Jasper <57488312+Jasper-27@users.noreply.github.com>
2022-10-27 15:55:20 +0100

    First API endpoint!
    Nodes can now check in! ... Not that this does anything

--- Server/server.go
+++ Server/server.go
@@ -1,22 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+type Command struct {
+	NodeID  string `json:"NodeID"`
+	Action  string `json:"Action"`
+	Content string `json:"Command"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Homepage endpoint hit")
+}
+
+type node_check_in struct {
+	ID string
+}
+
+func nodeCheckIn(w http.ResponseWriter, req *http.Request) {
+
+	dt := time.Now()
+	fmt.Println(dt.String())
+
+	// Sending a nice response
+	fmt.Fprint(w, "Node has checked in!")
+
+	// Decode the json body
+	decoder := json.NewDecoder(req.Body)
+	var node node_check_in
+	err := decoder.Decode(&node)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(node.ID + " Checked in")
+
+}
+
+func handleRequests() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+
+	// myRouter.HandleFunc("/test", shrug).Methods("GET")
+	myRouter.HandleFunc("/checkin", nodeCheckIn).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
 func main() {
+	handleRequests()
+}
 
-	http.HandleFunc("/home/jasper", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+func shrug() {
+	fmt.Println("🤷")
 }
