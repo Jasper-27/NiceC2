b49623f20c7fa3f48639a2ce818e4d0b52ec7704
Jasper <57488312+Jasper-27@users.noreply.github.com>
2022-12-16 16:59:22 +0000

    Slice of nodes
    There is a global slice which contains every node that has checked in. Including details such as their ID, hostname, Platform, first and last seen times

--- Server/server.go
+++ Server/server.go
@@ -37,6 +37,77 @@ type node_check_in struct {
 	Platform string
 }
 
+type node struct {
+	ID             string
+	Hostname       string
+	Platform       string
+	First_Check_In string
+	Last_Check_In  string
+}
+
+var nodes []node
+
+func createNode(ID string, Hostname string, Platform string, Timestamp string) node {
+
+	newNode := node{ID, Hostname, Platform, Timestamp, Timestamp}
+	return newNode
+
+}
+
+func is_new_node(input_ID string) bool {
+
+	for _, value := range nodes {
+		if value.ID == input_ID {
+			return false
+		}
+	}
+
+	return true
+}
+
+// updates the node in the list
+func update_node(ID string, Timestamp string) {
+
+	node_position, error := find_node(ID)
+	if error != "" {
+		fmt.Println(error)
+		return
+	}
+
+	nodes[node_position].Last_Check_In = Timestamp
+
+}
+
+// Finds a nodes position in the slice
+func find_node(input_ID string) (int, string) {
+
+	for i, value := range nodes {
+		if value.ID == input_ID {
+			return i, ""
+		}
+	}
+
+	// returns 0 if it can't find anything.
+	// pretty sure this small brain, but ehh
+	return 0, "💀 Couldn't find node"
+}
+
+func display_all_nodes() {
+
+	fmt.Println("")
+
+	fmt.Println("[][][][][][][][][] Nodes [][][][][][][][][]")
+	fmt.Println()
+	for _, value := range nodes {
+		fmt.Println("NodeID:        " + value.ID)
+		fmt.Println("Hostname:      " + value.Hostname)
+		fmt.Println("Platform:      " + value.Platform)
+		fmt.Println("First Seen:    " + value.First_Check_In)
+		fmt.Println("Last Seen:     " + value.Last_Check_In)
+		fmt.Println("------------------ ===== ------------------")
+	}
+}
+
 func nodeCheckIn(w http.ResponseWriter, req *http.Request) {
 
 	// Get's the current time
@@ -44,19 +115,31 @@ func nodeCheckIn(w http.ResponseWriter, req *http.Request) {
 
 	// Decode the json body
 	decoder := json.NewDecoder(req.Body)
-	var node node_check_in
-	err := decoder.Decode(&node)
+	var node_that_checked_in node_check_in
+	err := decoder.Decode(&node_that_checked_in)
 	if err != nil {
 		panic(err)
+	}
+
+	// Adding the node to the list (Needs to be only if new node)
+
+	if is_new_node(node_that_checked_in.ID) == true {
+		fmt.Println("Damn look it's a new node!")
+		nodes = append(nodes, createNode(node_that_checked_in.ID, node_that_checked_in.Hostname, node_that_checked_in.Platform, dt.String()))
+	} else {
+
+		update_node(node_that_checked_in.ID, dt.String())
+
+		fmt.Println("Node re-checked in")
 	}
 
 	// Output something pretty
 	fmt.Println()
 	fmt.Println("============= New Check in =============")
 
-	fmt.Println("ID: " + node.ID)
-	fmt.Println("Hostname: " + node.Hostname)
-	fmt.Println("Platform: " + node.Platform)
+	fmt.Println("ID: " + node_that_checked_in.ID)
+	fmt.Println("Hostname: " + node_that_checked_in.Hostname)
+	fmt.Println("Platform: " + node_that_checked_in.Platform)
 	fmt.Println("Time: " + dt.String())
 
 	fmt.Println("============= ----------- =============")
@@ -68,13 +151,17 @@ func nodeCheckIn(w http.ResponseWriter, req *http.Request) {
 
 	var response = []byte(`
 	{
-		"ID": "` + node.ID + `", 
+		"ID": "` + node_that_checked_in.ID + `", 
 		"command": "run", 
 		"details": "` + Command + `"
 	}`)
 
 	// Send the response back
 	fmt.Fprintf(w, string(response))
+
+	fmt.Println()
+
+	display_all_nodes()
 
 }
 
