78bcb32a4352e53f5405e9b0d807ba8ff2f452d4
Jasper <57488312+Jasper-27@users.noreply.github.com>
2023-02-01 20:47:12 +0000

    added some comments

--- Server/structs.go
+++ Server/structs.go
@@ -1,5 +1,6 @@
 package main
 
+// task sent to node to complete. and task stored in server
 type Task struct {
 	TaskID   string
 	NodeID   string
@@ -8,18 +9,21 @@ type Task struct {
 	Progress string
 }
 
+// Send to server when node completes response
 type Task_Response struct {
 	TaskID   string
 	Progress string // Completed / Failed
 	Result   string // Data from the task
 }
 
+// Data sent to server each time node checks in
 type check_in struct {
 	ID       string
 	Hostname string
 	Platform string
 }
 
+// Data stored about each node
 type node struct {
 	ID             string
 	Hostname       string
