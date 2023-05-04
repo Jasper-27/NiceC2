bbad48f2bb15bbb2b25cc56708ad2eeb277fbd61
Jasper <57488312+Jasper-27@users.noreply.github.com>
2023-05-04 20:18:09 +0100

    Proof of concept now runs as user on mac.
    The auto-start system on mac is weird. it doesn't run as root. it runs as the user that installs it. If installed as root, it would only run if the root user logs in. This is a pain, but not the end of the world. Does break some functionality  (like being able to remote shutdown).

--- Learning/Persistance/main.go
+++ Learning/Persistance/main.go
@@ -5,19 +5,43 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
+	"os/user"
 	"path/filepath"
 	"runtime"
+	"strings"
+
+	"github.com/emersion/go-autostart"
 )
 
 func main() {
+
+	runCommand("say Hello")
+
+	runCommand("touch /Users/jasper/Desktop/itWorked_txt")
 
 	log.Println("Starting")
 	err, destination_path := installSelf()
 	if err != nil {
 		fmt.Println(err)
+		// return
 	}
 
-	fmt.Println(destination_path)
+	log.Println("File created at: " + destination_path)
+
+	// THE AUTO START ISN"T WORKING!!!
+
+	// COULD IT BE BECAUE THE WRONG USER IS LOGGED IN?
+
+	// Removes the auto start if it already exists
+	remove_auto_start("sh", destination_path)
+
+	create_auto_start("sh", destination_path)
+
+	fmt.Println(check_enabled("sh", destination_path))
+
+	runCommand("say Hello")
+
 }
 
 func installSelf() (error, string) {
@@ -33,18 +57,37 @@ func installSelf() (error, string) {
 	case "linux":
 		dst = "/usr/local/bin/NiceC2/"
 	case "darwin":
-		dst = "/Library/Application Support/NiceC2/"
+		// dst = "/Library/Application Support/NiceC2/"
+
+		currentUser, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		dst = "/Users/" + currentUser.Username + "/Library/NiceC2/"
+
 	case "windows":
 		dst = filepath.Join(os.Getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup", "NiceC2")
 	default:
 		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS), ""
 	}
 
-	// Creates the directory for NiceC2 to go into
-	err1 := os.Mkdir(dst, 0755)
-	if err1 != nil {
-		fmt.Printf("Error creating directory %s: %s\n", dst, err1)
-		return err1, ""
+	// Check if the directory already exists
+	if _, err := os.Stat(dst); os.IsNotExist(err) {
+		// Create the directory with 0755 permissions (rwxr-xr-x)
+		err = os.Mkdir(dst, 0755)
+		if err != nil {
+			fmt.Printf("Error creating directory %s: %s\n", dst, err)
+			return err, ""
+		}
+
+		fmt.Printf("Directory %s created successfully\n", dst)
+	} else if err != nil {
+		// Handle other errors that occurred while checking if the directory exists
+		fmt.Printf("Error checking for directory %s: %s\n", dst, err)
+		return err, ""
+	} else {
+		// Directory already exists, do nothing
+		fmt.Printf("Directory %s already exists\n", dst)
 	}
 
 	// Open the destination file for writing.
@@ -77,4 +120,111 @@ func installSelf() (error, string) {
 	}
 
 	return nil, filepath.Join(dst, filepath.Base(self))
+}
+
+func check_enabled(shell string, commandString string) bool {
+	app := &autostart.App{
+		Name:        "NiceC2",
+		DisplayName: "NiceC2 command agent",
+		Exec:        []string{shell, "-c", commandString},
+	}
+
+	if app.IsEnabled() {
+		return true
+	} else {
+		return false
+	}
+}
+
+func create_auto_start(shell string, commandString string) {
+	app := &autostart.App{
+		Name:        "NiceC2",
+		DisplayName: "NiceC2 command agent",
+		Exec:        []string{shell, "-c", commandString},
+	}
+
+	if app.IsEnabled() {
+		log.Println("App is already enabled")
+
+		// if err := app.Disable(); err != nil {
+		// 	log.Fatal(err)
+		// }
+	} else {
+		log.Println("Enabling app...")
+
+		if err := app.Enable(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func remove_auto_start(shell string, commandString string) {
+	app := &autostart.App{
+		Name:        "NiceC2",
+		DisplayName: "NiceC2 command agent",
+		Exec:        []string{shell, "-c", commandString},
+	}
+
+	if app.IsEnabled() {
+		log.Println("Removing app")
+
+		if err := app.Disable(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("App is not endabled")
+	}
+}
+
+func runCommand(command string) (outString string, errorMessage string) {
+
+	var shell string
+	errorMessage = ""
+
+	// Selecting which shell to use
+	if runtime.GOOS == "windows" {
+		shell = "powershell.exe"
+	} else {
+		shell = "sh"
+	}
+
+	// Change directory
+	if strings.HasPrefix(command, "cd ") {
+
+		dir := command[3:] // get the first three chars
+
+		os.Chdir(dir)
+
+		log.Println(dir)
+
+		// run command, and if it causes an error create an error
+		out, err := exec.Command(shell, "-c", "pwd").Output()
+		if err != nil {
+			log.Println(err.Error())
+			// errorMessage = err.Error()
+			errorMessage = "There was an error executing. "
+
+			return
+		}
+
+		outString = string(out)
+		return
+	}
+
+	// run command, and if it causes an error create an error
+	out, err := exec.Command(shell, "-c", command).Output()
+	if err != nil {
+
+		fmt.Println("There was an error. Ohh dear")
+		log.Println(err.Error())
+		errorMessage = "There was an error running the command"
+
+		return
+	}
+
+	outString = string(out)
+
+	return
+
 }
