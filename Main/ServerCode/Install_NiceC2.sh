79196f7921b5daa80c9cb1a981ded21f6c16e2e2
Jasper <57488312+Jasper-27@users.noreply.github.com>
2023-05-06 00:54:19 +0100

    Update Install_NiceC2.sh

--- Main/ServerCode/Install_NiceC2.sh
+++ Main/ServerCode/Install_NiceC2.sh
@@ -3,7 +3,13 @@
 
 echo THANKS FOR CHOOSING NICEC2 - JASPER 
                                              
+## Install the server code
+cd Server
+sh install.sh
 
-sh Server/install.sh
+## Get back 
+cd ..
+
+## Install the Console code 
 
 sh Console/install.sh
